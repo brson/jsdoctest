6b9c72937474fe0e5b44eb0f414c4d7ba02d4aa5
Brian Anderson <andersrb@gmail.com>
2018-04-14 17:49:48 -0700

    wip

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 [check this out](javascript:alert\(1\))
+
+[and this]
+
+[and this]: javascript:alert\(1\)
+
+![and this image](javascript:alert\(1\))
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 //! [check this out](javascript:alert\(1\))
+//!
+//! [and this]
+//!
+//! [and this]: javascript:alert\(1\)
+//!
+//! ![and this image](javascript:alert\(1\))
 
 #[cfg(test)]
 mod tests {
